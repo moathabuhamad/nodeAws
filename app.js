648723277a372bc42bcd6e4811ab7430af858012
moathabuhamad <moathabuhamad@outlook.com>
2022-03-30 20:08:07 +0300

    test123

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+
+const port = process.env.PORT || 3000;
+
 app.listen(3000,()=>{
-    console.log("Listening to port 3000")
+    console.log("Listening to port 3000");
 });
 
 app.get('/',(req,res)=>{
